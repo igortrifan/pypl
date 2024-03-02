ba6219f1e58d0761552ee1776b14e7fe8e3f447d
igortrifan <38112229+igortrifan@users.noreply.github.com>
2024-03-02 12:56:55 +0200

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prof-library",
+    version="0.0.1",
+    author="igortrifan",
+    author_email="trifan.igor.xd@gmail.com",
+    url="",
+    description="calculate number of trees",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    entry_points={"console_scripts": ["proj-library = proj-library.main:download"]},
+)
